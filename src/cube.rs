8d4ab4988c3609fb9f1393fdf21687633889334f
Dave O <davidobrite@gmail.com>
2014-10-15 05:52:23 -0700

    relabel axis in comments

--- src/cube.rs
+++ src/cube.rs
@@ -1,14 +1,14 @@
 use renderer::CubeVertex;
 
 pub fn create_cube(x: f32, z: f32, y: f32, buffer: &mut Vec<CubeVertex>) {
-    //back (0, 0, 1)
+    //front (0, 0, 1)
     buffer.push(CubeVertex::new([0.0f32 + x, 0.0f32 + y, 1.0f32 + z], [0f32, 0f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([1.0f32 + x, 0.0f32 + y, 1.0f32 + z], [1f32, 0f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([1.0f32 + x, 1.0f32 + y, 1.0f32 + z], [1f32, 1f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([1.0f32 + x, 1.0f32 + y, 1.0f32 + z], [1f32, 1f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([0.0f32 + x, 1.0f32 + y, 1.0f32 + z], [0f32, 1f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([0.0f32 + x, 0.0f32 + y, 1.0f32 + z], [0f32, 0f32], [255f32, 255f32, 255f32]));
-    //front (0, 0, 0)
+    //back (0, 0, 0)
     buffer.push(CubeVertex::new([1.0f32 + x, 1.0f32 + y, 0.0f32 + z], [0f32, 0f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([0.0f32 + x, 1.0f32 + y, 0.0f32 + z], [1f32, 0f32], [255f32, 255f32, 255f32]));
     buffer.push(CubeVertex::new([0.0f32 + x, 0.0f32 + y, 0.0f32 + z], [1f32, 1f32], [255f32, 255f32, 255f32]));
